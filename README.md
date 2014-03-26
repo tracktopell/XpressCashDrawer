4a6589c3b167eee37d47ecd7ec459db03bf3ee5a
alfredo <alfredo@Xubuntu64-VB>
2014-03-26 13:11:01 -0600

    WEBSITE INDEX.PHP ADDED

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-XpressCashDrawer
-================
+						XpressCashDrawer
+						================
 
 Xpressosystems Cash Drawer Minimalistic App 4 little retail app
+
+LOCAL building.
+
+	mvn clean package -Pinstaller
+
+RUN from here:
+
+	mvn clean exec:java -Dexec.mainClass=com.xpressosystems.xpresscashdrawer.Main
+
+Update & Upload the installer int the website
+
+	mvn clean install -Pinstaller
+
---
+++ src/main/resources/index.php
@@ -0,0 +1,70 @@
+<html>
+	<head>
+	 <style type="text/css">
+	  	h1 {
+			text-align:center;			
+			background-color:#b0e0e6;
+		}
+		h2 {
+			text-align:center;						
+		}
+		h3 {
+			text-align:center;			
+			background-color:#b0e0ae;
+		}
+		.trImg
+		{
+			padding:15px;
+		}
+		.tdImg
+		{
+			padding:15px;
+		}
+	  </style>
+	</head>
+
+	<body>
+		<h1>XpressCashdrawer ${project.version}</h1>
+		<h2>Sistema de Caja para tiendas de abarrotes en M&eacute;xico</h2>			
+		<table >
+			<tr>
+				<td width="60%" valign="top">
+					<table border="0">
+						<tr class="trImg"><td class="tdImg"><img src="imgs/xcd_1.png" width="100%" height="100%"/></td><td class="tdImg"><img src="imgs/xcd_2.png" width="100%" height="100%"/></td></tr>
+						<tr class="trImg"><td class="tdImg"><img src="imgs/xcd_3.png" width="100%" height="100%"/></td><td class="tdImg"><img src="imgs/xcd_4.png" width="100%" height="100%"/></td></tr>
+					</table>
+				</td>
+				<td	valign="top">
+						<h3>OBJETIVO:</h3>
+							<p> 
+							...Ofrece las funciones b&aacute;sicas de venta de articulos y mantenimiento de invetario.
+							<br/> 
+							La impresi&oacute;n de Tickets a impresoras modo texto por BlueTooth.
+							<br/> 
+							Libre de mantenimiento con auto-actualizaci&oacute;n para posteriores versiones.
+							</p>
+						<h3>DESCARGA:</h3>
+						<p>
+							Seleccione gardar el <a href="./${project.name}-standard.jar">Instalador Java</a>
+							y ejecute en linea de comando: <br/>
+							java -jar ${project.name}-standard.jar 
+							<br/>
+							o si esta bien isntalado su JVM, solo de dobleclick al archivo.<br/>
+							Esto debera iniciar el Asistente de instalaci&oacute;n.
+						</p>						
+						<h3>EMPEZAR A USAR:</h3>  
+						<p>		
+							Hay 2 Roles, administrador y usuario com&uacute;n, 
+							la contrase&ntilde;a por default para cada una es: abc$  y 123 respectivamente.
+						</p>
+						<h3>ACERCA DE:</h3>  
+						<p>		
+							Esta es una version de borrador de distribuci&oacute;n de XpressCashdrawer.
+							Mantenida por Alfredo Estrada (tracktopell@gmail.com).
+						</p>						
+				</td>
+			</tr>
+		<table>
+
+	</body>
+</html>
